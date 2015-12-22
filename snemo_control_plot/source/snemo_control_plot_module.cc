d6b2b17c7d706704a4ae161bf06b05790a315ce6
Xavier Garrido <xavier.garrido@gmail.com>
2015-12-22 17:53:15 +0100

    propagate change within histogram service

--- snemo_control_plot/source/snemo_control_plot_module.cc
+++ snemo_control_plot/source/snemo_control_plot_module.cc
@@ -80,7 +80,7 @@ namespace analysis {
                   std::logic_error,
                   "Module '" << get_name() << "' has no '" << histogram_label << "' service !");
       dpp::histogram_service & Histo
-        = service_manager_.get<dpp::histogram_service>(histogram_label);
+        = service_manager_.grab<dpp::histogram_service>(histogram_label);
       set_histogram_pool(Histo.grab_pool());
       if (config_.has_key("Histo.output_files")) {
         std::vector<std::string> output_files;
